7e20ff8e5fd9f4815ae31b9c51797626c702d672
Gordon <gordonzhou68@outlook.com>
2019-10-18 21:37:09 -0400

    apply css style to blocks, add router links to them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 const routes: Routes = [
+  //This route redirects a URL that fully matches the empty path to the route whose path is '/dashboard'
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  //This route redirects a URL that fully matches the empty path to the route whose path is '/dashboard'
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: 'detail/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
